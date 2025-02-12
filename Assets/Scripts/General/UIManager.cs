24420a904f61f50496c83050a0c25409d3a533b6
Eduard Minguell <60965207+Eduardiko@users.noreply.github.com>
2025-02-12 17:30:55 +0100

    Grades

--- Assets/Scripts/General/UIManager.cs
+++ Assets/Scripts/General/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 using TMPro;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -13,11 +14,16 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Transform healthUIGroup;
     [SerializeField] private TextMeshProUGUI scoreText;
 
+    [SerializeField] private Image gradesObject;
+    [SerializeField] private Sprite[] grades;
+
     [HideInInspector] public static int playerScore = 0;
 
     private void Update()
     {
         scoreText.text = "Score: " + playerScore;
+
+        UpdateGrade();
 
         if (playerPrefab == null)
             endLevelMenu.SetActive(true);
@@ -31,5 +37,39 @@ public class UIManager : MonoBehaviour
         {
             GameObject.Instantiate(healthUIPrefab, healthUIGroup);
         }
+    }
+
+    private void UpdateGrade()
+    {
+        int i = 0;
+
+        if (playerScore < 175)
+            i = 0;
+        else if (playerScore >= 175 && playerScore < 350)
+            i = 1;
+        else if (playerScore >= 350 && playerScore < 525)
+            i = 2;
+        else if (playerScore >= 525 && playerScore < 700)
+            i = 3;
+        else if (playerScore >= 700 && playerScore < 875)
+            i = 4;
+        else if (playerScore >= 875 && playerScore < 1050)
+            i = 5;
+        else if (playerScore >= 1050 && playerScore < 1225)
+            i = 6;
+        else if (playerScore >= 1225 && playerScore < 1400)
+            i = 7;
+        else if (playerScore >= 1400 && playerScore < 1575)
+            i = 8;
+        else if (playerScore >= 1575 && playerScore < 1750)
+            i = 9;
+        else if (playerScore >= 1750 && playerScore < 1925)
+            i = 10;
+        else if (playerScore >= 1925 && playerScore < 2275)
+            i = 11;
+        else if (playerScore >= 2275)
+            i = 12;
+        
+        gradesObject.sprite = grades[i];
     }
 }
